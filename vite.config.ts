723400ea1d7ccb65bb48e72391f5b4af7a7c4be1
ezzalan54 <155285356+ezzalan54@users.noreply.github.com>
2025-02-16 23:30:45 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: './',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
